23678478835982bea24e8f5e487315c547e421da
Jayanta-kumar-paul <kumarpaul912@gmail.com>
2024-11-13 21:20:23 +0530

    modify

--- 2nd-Semmenter/Q14.cpp
+++ 2nd-Semmenter/Q14.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 using namespace std;
 
-void Circle(int radius, double& area, double& circumference) 
+void Circle(int radius, double &area, double &circumference)
 {
-    double pi = 3.14;  
-    area = pi * radius * radius;  
-    circumference = 2 * pi * radius;  
+    double pi = 3.14;
+    area = pi * radius * radius;
+    circumference = 2 * pi * radius;
 }
 
-int main() {
+int main()
+{
     int radius;
-    double area, circumference;  
+    double area, circumference;
 
-    cout << "Enter the radius of the circle: ";
+    cout << "Enter the radius of the circle: ";  
     cin >> radius;
 
     Circle(radius, area, circumference);
@@ -22,3 +23,6 @@ int main() {
 
     return 0;
 }
+// Enter the radius of the circle: 5
+// Area of the circle: 78.5
+// Circumference of the circle: 31.4
